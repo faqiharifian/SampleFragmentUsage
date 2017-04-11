e661f666cdc3587da92b8fa9bc19dffafc922194
Faqih Arifian <faqiharifianaji@gmail.com>
2017-04-12 05:42:58 +0700

    finish

--- app/src/main/java/com/arifian/dicoding/samplefragmentusage/MainActivity.java
+++ app/src/main/java/com/arifian/dicoding/samplefragmentusage/MainActivity.java
@@ -1,7 +1,9 @@
 package com.arifian.dicoding.samplefragmentusage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.arifian.dicoding.samplefragmentusage.fragment.FirstFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.frame_container,
+                            new FirstFragment(),
+                            FirstFragment.class.getSimpleName()).commit();
+        }
     }
 }
